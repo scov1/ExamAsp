5b8534b78354c441be125a03c6961ff6dff47e7e
муружеваС <@DESKTOP-GTSSVCO>
2019-10-24 20:55:10 +0600

    addAnimation

--- ExamAsp/App_Start/BundleConfig.cs
+++ ExamAsp/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace ExamAsp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/style.css"));
         }
     }
 }
